f4a178995196967157c7d03b750cd41f0a0698c4
DHARSHAN, RAVICHANDR <ravichandr.dharshan@accenture.com>
2024-04-24 22:41:51 +0530

    Function app code

---
+++ BlobTriggerFunction.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionAppBlob
+{
+    public class BlobTriggerFunction
+	{
+        [FunctionName("BlobTriggerFunction")]
+        public void Run([BlobTrigger("anchortransform/anchordata.json", Connection = "BlobStorageConnectionString")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
